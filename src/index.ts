5821269951fc673bed5662263595e1eeb9881420
Alex Linden <alextl2004@gmail.com>
2023-02-15 13:09:04 -0800

    basic typing and spellcheck

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
 let id: number = 5;
+let company: string = "Company";
+let isPublished: boolean = true;
+let x: any = "any";
+
+let ids: number[] = [1, 2, 3, 4];
 
 console.log('ID: ', id);
+
+// ids.push("g"); wont work
+
+let arr: any[] = [1, 2, "hello", "world"] // can have any type
+
+//Tuple
+let person: [number, string, boolean] = [1, "brad", true]
+
+//Tuple Array
+let employee: [number, string][]
+
+employee = [
+  [1, "me"],
+  [2, "you"],
+  ["them", 3] // won't work because in wrong order
+]
